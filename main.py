c9b8701a34a6ce8b15a8eab14493fa16fc61665b
Jordan Low <jordanlow.jinyi@gmail.com>
2020-07-03 15:20:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from chess import Board, King, Queen, Bishop, Knight, Rook, Pawn
 
 game = Board()
-# game.start()
-# while game.winner is None:
-#     game.display()
-#     start, end = game.prompt()
-#     game.update(start, end)
-#     game.next_turn()
-# print(f'Game over. {game.winner} player wins!')
-game.add((0,1),Rook('black'))
-game.add((0,2),King('white'))
-game.display()
-game.update((0,1),(0,2))
-game.display()
-game.winner
+game.start()
+while game.winner is None:
+    game.display()
+    start, end = game.prompt()
+    game.update(start, end)
+    game.next_turn()
+print(f'Game over. {game.winner} player wins!')
