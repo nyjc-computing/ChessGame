618a81d154db80d27c159bfd80b25ee505ff3b93
JS Ng <ng_jun_siang@moe.edu.sg>
2020-07-14 07:42:23 +0000

    resolve merge conflict

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from chess import Board, King, Queen, Bishop, Knight, Rook, Pawn
 
-game = Board()
-game.start()
-while game.winner is None:
-    game.display()
-    start, end = game.prompt()
-    game.update(start, end)
-    game.next_turn()
+game = Board(debug=True)
+if game.debug:
+    game.start()
+    while game.winner is None:
+        print('== DISPLAY ==')
+        game.display()
+        print('== PROMPT ==')
+        start, end = game.prompt()
+        print('== UPDATE ==')
+        game.update(start, end)
+        print('== NEXT TURN ==')
+        game.next_turn()
+else:
+    game.start()
+    while game.winner is None:
+        game.display()
+        start, end = game.prompt()
+        game.update(start, end)
+        game.next_turn()
 print(f'Game over. {game.winner} player wins!')
