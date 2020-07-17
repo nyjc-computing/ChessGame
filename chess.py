5fbb164ae5702be810ec3419b6de9c6b78c90720
Jordan Low <jordanlow.jinyi@gmail.com>
2020-07-17 07:29:45 +0000

    Refactored `promote()` out of `move()`

--- chess.py
+++ chess.py
@@ -58,15 +58,15 @@ class Board:
         Promote the 'Pawn' piece to a 'Queen' piece when the 'Pawn' piece reaches the opposite end
         '''
         if self.get_piece(end).name == "pawn" and (end[1] == 0 or end[1] == 7):
-            if self.get_piece(end).colour == 'black':
-                self.remove(end)
-                self.add(end,Queen('black'))
-            else:
-                self.remove(end)
-                self.add(end,Queen('white'))
+            self.promote(self.get_piece(end), end)
 
-        
-        
+    def promote(self, piece, end):
+        if piece.colour == 'black':
+            self.remove(end)
+            self.add(end,Queen('black'))
+        else:
+            self.remove(end)
+            self.add(end,Queen('white')) 
 
     def start(self):
         '''Set up the pieces and start the game.'''
